7750e4a52c2b327061984feac1085c2eaa9ebe25
Taylor Mohney <taylormohneytech@gmail.com>
2024-09-23 14:31:41 -0700

    Remove submodule and re-add frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CardList from './components/CardList';
+
+function App() {
+  return (
+    <div className="App">
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
